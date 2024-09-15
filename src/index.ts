f2eb8e44e3545e9c69ac9219eed07a455656720b
Tim Jones <timjonesuk@yahoo.co.uk>
2024-09-15 22:04:44 +0100

    testing ci

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,3 @@ import { Dictionary } from "./Dictionary";
 export { Dictionary } from "./Dictionary";
 export { Options } from "./types/Options.type";
 export { WordMetadata } from "./types/WordMetadata.type";
-
-
-const test = new Dictionary();
-console.log(test.find('disaster'));
