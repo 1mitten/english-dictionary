736ff9fdd4dae1d7e64dda422a6bdc0885503ac4
1mitten <tim4thstreet@gmail.com>
2024-09-16 16:33:45 +0100

    cors workaround for now

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-export { Dictionary } from "./Dictionary";
-export { Options } from "./types/Options.type";
-export { WordMetadata } from "./types/WordMetadata.type";
+import express from 'express';
+import DictionaryRoutes from './routes/DictionaryRoutes';
+const cors = require('cors');
+
+// Initialize the Express app
+const app = express();
+const port = 3000;
+
+// Create an instance of the DictionaryRoutes with an InMemoryProvide
+
+// Middleware to parse JSON bodies
+app.use(cors());
+app.use(express.json());
+
+// Use the dictionary routes
+app.use('/api/dictionary', DictionaryRoutes);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
