030d38e341840e7f991c5b67dd4931f37654490d
1mitten <tim4thstreet@gmail.com>
2024-09-16 15:24:00 +0100

    remove index code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,3 @@
-import { Dictionary } from "./Dictionary";
-import { InMemoryProvider } from "./InMemoryProvider";
-
 export { Dictionary } from "./Dictionary";
 export { Options } from "./types/Options.type";
 export { WordMetadata } from "./types/WordMetadata.type";
-
-const dict = new Dictionary();
-async function start() {
-    const words = await dict.findMany(['jesus', 'democracy', 'flabbergasted']);
-    console.log(words);
-}
-
-
-start();
